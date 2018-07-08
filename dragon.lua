5967154fb3820f6c77ecc6c531bcf35e636d63e9
Matteus <post@kalis.no>
2018-07-08 23:51:03 +0200

    Partial

--- dragon.lua
+++ dragon.lua
@@ -1,3 +1,14 @@
+local inspect = require('lib/inspect')
+local timer = require('lib/hump/timer')
+local console = require('console')(inspect)
+
+-- constants
+local ROTATION_DOWNWARD_CHANGE = 1.1 -- radians per second
+local ROTATION_MIN_ALLOWED = -0.6 -- radians
+local ROTATION_MAX_ALLOWED = 1 -- radians
+local GRAVITY = 700 -- acceleration down
+local JUMP_SPEED = 500 -- speed change up
+
 local dragonImage
 local dragonQuads = {}
 local dragonY = 100 -- pixels -- Starting position
@@ -5,8 +16,8 @@ local dragonRotation = ROTATION_MIN_ALLOWED -- Initial rotation
 local dragonSpeedY = 0 -- pixels per second
 local dragonCurrentQuad -- Quad
 
-local dragon = {
-  load = function()
+local dragon = {}
+dragon.load = function()
     -- Load image from file
     -- love.graphics.newImage(filename) -> Image
     dragonImage = love.graphics.newImage('assets/dragon/spritesheet.png')
@@ -20,5 +31,79 @@ local dragon = {
 
     -- initial quad
     dragonCurrentQuad = dragonQuads[1]
+end
+
+  update = function(dt)
+    -- Increase downwards speed with gravitation
+    dragonSpeedY = dragonSpeedY + (GRAVITY * dt)
+    -- Change postition according to current speed downwards
+    dragonY = dragonY + (dragonSpeedY * dt)
+
+    -- Change rotation when freefalling down, until max rotation reached
+    if dragonRotation < ROTATION_MAX_ALLOWED then
+      dragonRotation = dragonRotation + (ROTATION_DOWNWARD_CHANGE * dt)
+    end
+
+    console.log('dragonY', dragonY)
+    console.log('dragonRotation', dragonRotation)
+
+    -- Activate timer library
+    timer.update(dt)
+  end,
+  draw = function()
+    -- Set color used for drawing
+    -- love.graphics.setColor(red, green, blue, alfa)
+    love.graphics.setColor(0.14, 0.36, 0.46)
+
+    -- Draw rectangle on screen
+    -- love.graphics.rectangle(mode, x, y, width, height)
+    love.graphics.rectangle('fill', 0, 0, love.graphics.getWidth(), love.graphics.getHeight())
+
+    -- Set color used for drawing
+    -- love.graphics.setColor(red, green, blue, alfa)
+    love.graphics.setColor(1, 1, 1)
+
+    -- Draw a drawable object into the screen
+    -- love.graphics.draw(drawable, [quad], x, y, rotation, scaleFactorX, scaleFactorY, originOffsetX, originOffsetY)
+    love.graphics.draw(dragonImage, dragonCurrentQuad, 100, dragonY, dragonRotation, 0.3, 0.3, 250, 250)
+
+    -- Activate console library
+    console.draw()
+  end,
+  keypressedSpace = function()
+    -- Add "jump" upwards
+    dragonSpeedY = -JUMP_SPEED
+
+    -- Do the rotation to initial position with some frames
+    -- Angle in radians, between current rotation, and minimal allowed rotation
+    local sum = math.abs(dragonRotation) + math.abs(ROTATION_MIN_ALLOWED)
+     -- Step is radians to change for each frame.
+    local step = sum / 16
+    timer.script(function(wait)
+      -- Abort when current rotation reaches minimal allowed rotation
+      while dragonRotation > ROTATION_MIN_ALLOWED do
+        -- Change dragonRotation
+        dragonRotation = dragonRotation - step
+        -- Make tween effect
+        wait(0.001)
+      end
+    end)
+
+    -- Change quads when flying up with the dragon
+    timer.script(function(wait)
+      dragonCurrentQuad = dragonQuads[2]
+      wait(0.1)
+      dragonCurrentQuad = dragonQuads[3]
+      wait(0.1)
+      dragonCurrentQuad = dragonQuads[4]
+      wait(0.05)
+      dragonCurrentQuad = dragonQuads[3]
+      wait(0.05)
+      dragonCurrentQuad = dragonQuads[2]
+      wait(0.05)
+      dragonCurrentQuad = dragonQuads[1]
+    end)
   end,
 }
+
+return dragon
--- main.lua
+++ main.lua
@@ -1,80 +1,25 @@
 -- libraries
 local inspect = require('lib/inspect')
 local console = require('console')(inspect)
-local timer = require('lib/hump/timer')
 
--- constants
-local ROTATION_DOWNWARD_CHANGE = 1.1 -- radians per second
-local ROTATION_MIN_ALLOWED = -0.6 -- radians
-local ROTATION_MAX_ALLOWED = 1 -- radians
-local GRAVITY = 700 -- acceleration down
-local JUMP_SPEED = 500 -- speed change up
-
--- variables
-local dragonImage
-local dragonQuads = {}
-local dragonY = 100 -- pixels -- Starting position
-local dragonRotation = ROTATION_MIN_ALLOWED -- Initial rotation
-local dragonSpeedY = 0 -- pixels per second
-local dragonCurrentQuad -- Quad
+-- modules
+local dragon = require('dragon')
 
 function love.load()
   -- Sets display mode and properties of window
   -- love.window.setMode(width, height, flagsTable)
   love.window.setMode(1200, 700)
 
-  -- Load image from file
-  -- love.graphics.newImage(filename) -> Image
-  dragonImage = love.graphics.newImage('assets/dragon/spritesheet.png')
-
-  -- Define quad for dragonImage
-  -- love.graphics.newQuad(x, y, width, height, sheetWidth, sheetHeight) -> Quad
-  dragonQuads[1] = love.graphics.newQuad(0, 0, 500, 500, dragonImage:getDimensions())
-  dragonQuads[2] = love.graphics.newQuad(500, 0, 500, 500, dragonImage:getDimensions())
-  dragonQuads[3] = love.graphics.newQuad(1000, 0, 500, 500, dragonImage:getDimensions())
-  dragonQuads[4] = love.graphics.newQuad(1500, 0, 500, 500, dragonImage:getDimensions())
-
-  -- initial quad
-  dragonCurrentQuad = dragonQuads[1]
+  dragon.load()
 end
 
 function love.update(dt)
-  -- Increase downwards speed with gravitation
-  dragonSpeedY = dragonSpeedY + (GRAVITY * dt)
-  -- Change postition according to current speed downwards
-  dragonY = dragonY + (dragonSpeedY * dt)
+  dragon.update(dt)
 
-  -- Change rotation when freefalling down, until max rotation reached
-  if dragonRotation < ROTATION_MAX_ALLOWED then
-    dragonRotation = dragonRotation + (ROTATION_DOWNWARD_CHANGE * dt)
-  end
-
-  console.log('dragonY', dragonY)
-  console.log('dragonRotation', dragonRotation)
-
-  -- Activate timer library
-  timer.update(dt)
 end
 
 function love.draw()
-  -- Set color used for drawing
-  -- love.graphics.setColor(red, green, blue, alfa)
-  love.graphics.setColor(0.14, 0.36, 0.46)
-
-  -- Draw rectangle on screen
-  -- love.graphics.rectangle(mode, x, y, width, height)
-  love.graphics.rectangle('fill', 0, 0, love.graphics.getWidth(), love.graphics.getHeight())
-
-  -- Set color used for drawing
-  -- love.graphics.setColor(red, green, blue, alfa)
-  love.graphics.setColor(1, 1, 1)
-
-  -- Draw a drawable object into the screen
-  -- love.graphics.draw(drawable, [quad], x, y, rotation, scaleFactorX, scaleFactorY, originOffsetX, originOffsetY)
-  love.graphics.draw(dragonImage, dragonCurrentQuad, 100, dragonY, dragonRotation, 0.3, 0.3, 250, 250)
-
-  -- Activate console library
-  console.draw()
+  dragon.draw()
 end
 
 function love.keypressed(key)
@@ -85,37 +30,6 @@ function love.keypressed(key)
 
   -- Push space to fly up with dragon
   if key == 'space' then
-    -- Add "jump" upwards
-    dragonSpeedY = -JUMP_SPEED
-
-    -- Do the rotation to initial position with some frames
-    -- Angle in radians, between current rotation, and minimal allowed rotation
-    local sum = math.abs(dragonRotation) + math.abs(ROTATION_MIN_ALLOWED)
-     -- Step is radians to change for each frame.
-    local step = sum / 16
-    timer.script(function(wait)
-      -- Abort when current rotation reaches minimal allowed rotation
-      while dragonRotation > ROTATION_MIN_ALLOWED do
-        -- Change dragonRotation
-        dragonRotation = dragonRotation - step
-        -- Make tween effect
-        wait(0.001)
-      end
-    end)
-
-    -- Change quads when flying up with the dragon
-    timer.script(function(wait)
-      dragonCurrentQuad = dragonQuads[2]
-      wait(0.1)
-      dragonCurrentQuad = dragonQuads[3]
-      wait(0.1)
-      dragonCurrentQuad = dragonQuads[4]
-      wait(0.05)
-      dragonCurrentQuad = dragonQuads[3]
-      wait(0.05)
-      dragonCurrentQuad = dragonQuads[2]
-      wait(0.05)
-      dragonCurrentQuad = dragonQuads[1]
-    end)
+    dragon.keypressedSpace()
   end
 end
