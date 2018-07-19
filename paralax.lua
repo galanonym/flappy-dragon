97fdf0a4633cf74a04ab84e6037eef04cd61e077
Matteus <post@kalis.no>
2018-07-20 00:27:54 +0200

    Add mountines to paralax module

--- paralax.lua
+++ paralax.lua
@@ -4,9 +4,16 @@ return function()
   -- variables
   local cloudImage
   local cloudX = 0
-  local cloudScale = 0.3
-  local cloudSpeed = 30
+  local cloudScale = 0.4
+  local cloudSpeed = 20
   local cloudLoopPoint = 3840 * cloudScale
+
+  local mountineImage
+  local mountineX = 0
+  local mountineY = 350
+  local mountineScale = 0.7
+  local mountineSpeed = 40
+  local mountineLoopPoint = 3840 * mountineScale
 
   -- main module object
   local paralax = {}
@@ -14,6 +21,7 @@ return function()
   -- LOVE functions
   paralax.load = function()
     cloudImage = love.graphics.newImage('assets/clouds/spritesheet.png')
+    mountineImage = love.graphics.newImage('assets/mounitnes/spritesheet.png')
   end -- load
 
   paralax.update = function(dt)
@@ -22,6 +30,10 @@ return function()
       cloudX = 3
     end
 
+    mountineX = (mountineX + mountineSpeed * dt)
+    if mountineX > mountineLoopPoint then
+      mountineX = 3
+    end
   end -- update
 
   paralax.draw = function()
@@ -31,6 +43,7 @@ return function()
     -- draw the background at the negative looping point
     -- love.graphics.draw(drawable, [quad], x, y, rotation, scaleFactorX, scaleFactorY, originOffsetX, originOffsetY)
     love.graphics.draw(cloudImage, -cloudX, 0, 0, cloudScale, cloudScale)
+    love.graphics.draw(mountineImage, -mountineX, mountineY, 0, mountineScale, mountineScale)
   end -- draw
 
   return paralax
