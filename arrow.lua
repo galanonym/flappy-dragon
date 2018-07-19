7ec9d200644d56423740c2968ac5f63e988a4ee0
Piotrek <ludzik3217@gmail.com>
2018-07-19 23:43:02 +0200

    Add physics to arrow

--- arrow.lua
+++ arrow.lua
@@ -5,9 +5,6 @@ return function(console)
   -- constants
 
   -- variables
-  -- @todo check where is it in code
-  -- local arrowSpeedX = 700 -- pixels per second
-
   local arrowImage
   local arrowImageWidth = 500
   local arrowImageHeight = 500
@@ -15,26 +12,24 @@ return function(console)
   local arrowY = math.random(0, 400) -- pixels -- Starting position
   local arrowRotation = 0
   local arrowScale = 0.1
+  local arrowIsShot = false
 
 
-    local arrowPolygons = {}
-    arrowPolygons[1] = {
-      -- head
-      193.00,300.00,
-      160.00,306.00,
-      33.00,198.00,
-      143.00,230.00,
-    }
-    -- arrowPolygons[2] = {
-    --   -- stomac
-    -- }
-    -- arrowPolygons[3] = {
-    --   -- tail
-    -- }
+  local arrowPolygons = {}
+  arrowPolygons[1] = {
+    -- head
+    359.00,311.00,
+    417.00,341.00,
+    305.00,393.00,
+    57.00,333.00,
+    119.00,308.00,
+    201.00,304.00,
+  }
 
   -- main module object
   local arrow = {}
 
+  -- LOVE functions
   arrow.load = function(world)
     -- Load image from file
     -- love.graphics.newImage(filename) -> Image
@@ -52,22 +47,17 @@ return function(console)
   end -- load
 
   arrow.update = function()
-    -- Fixed x position of the dragon
-    -- dragonPhysics.getBody():setX(dragonX)
-
     -- Update according to physics model
     arrowY = arrowPhysics.getBody():getY()
-    --     -- Change postition according to current speed downwards
-    --     arrowX = arrowX - (arrowSpeedX * dt)
-    --     if arrowX < -arrowWidthScaled then
-    --       arrowX = 1000
-    --       arrowY = math.random(0, 400)
-    --       arrowSpeedX = math.random(300, 700)
-    --     end
-    --     -- update arrowX
-    --     arrowBody:setX(arrowX)
-    --     arrowBody:setY(arrowY)
-    --   end
+    -- Update according to physics model
+    arrowX = arrowPhysics.getBody():getX()
+
+    -- Initial arrow shot
+    if arrowIsShot == false then
+      -- Body:applyLinearImpulse( ix, iy )
+      arrowPhysics.getBody():applyLinearImpulse(-50, 0)
+      arrowIsShot = true
+    end
 
     console.log('arrowY', arrowY)
   end -- update
@@ -98,53 +88,4 @@ return function(console)
   end -- draw
 
   return arrow
-end
-
---   -- dragon physics variables
---   local arrowBody
---   local arrowShape
---   local arrowFixture
---
---   local arrow = {}
---
---   arrow.load = function(world)
---     arrowImage = love.graphics.newImage('assets/arrow/spritesheet.png')
---     arrowWidthScaled = math.ceil(arrowImage:getWidth() * 0.1)
---
---     -- define physics variables
---     -- body = love.physics.newBody( world, x, y, type )
---     arrowBody = love.physics.newBody(world, arrowX, arrowY, 'dynamic')
---     -- shape = love.physics.newRectangleShape( width, height )
---     arrowShape = love.physics.newRectangleShape(100, 50)
---     -- fixture = love.physics.newFixture( body, shape, density )
---     arrowFixture = love.physics.newFixture(arrowBody, arrowShape)
---     console.log('arrowWidthScaled', arrowWidthScaled)
---   end
---
---   arrow.update = function(dt)
---     -- Change postition according to current speed downwards
---     arrowX = arrowX - (arrowSpeedX * dt)
---     if arrowX < -arrowWidthScaled then
---       arrowX = 1000
---       arrowY = math.random(0, 400)
---       arrowSpeedX = math.random(300, 700)
---     end
---     -- update arrowX
---     arrowBody:setX(arrowX)
---     arrowBody:setY(arrowY)
---   end
---
---   arrow.draw = function()
---     -- love.graphics.setColor( red, green, blue, alpha )
---     love.graphics.setColor(1, 1, 1)
---
---     -- Draw a drawable object into the screen
---     -- love.graphics.draw(drawable, [quad], x, y, rotation, scaleFactorX, scaleFactorY, originOffsetX, originOffsetY)
---     love.graphics.draw(arrowImage, arrowX, arrowY, 0, 0.1, 0.1)
---
---     love.graphics.setColor(0.28, 0.63, 0.05)
---     love.graphics.polygon("fill", arrowBody:getWorldPoints(arrowShape:getPoints()))
---   end
---
---   return arrow
--- end
+end -- closure
